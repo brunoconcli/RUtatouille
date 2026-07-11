80ffa00561e4fa91714044691d65545f2bd9ad7d
brunoconcli <brunoconcli@gmail.com>
2026-07-10 22:23:22 -0300

    wip: Criação dos models

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,22 +1,22 @@
 public class Usuario
 {
   public string Codigo { get; set; }
-  private string Email { get; set; }
-  private string Nome { get; set; }
-  private double Extrato { get; set; }
+  public string Email { get; private set; }
+  public string Nome { get; private set; }
+  public double Extrato { get; private set; }
 
-  public Usuario CriarUsuario(string codigo, string email, string nome)
+  public Usuario(string codigo, string email, string nome)
   {
-    return new Usuario()
-    {
-      Codigo = codigo,
-      Email = email,
-      Nome = nome,
-      Extrato = 0.0
-    };
+    Codigo = codigo;
+    Email = email;
+    Nome = nome;
+    Extrato = 0.0;
   }
+  
   public void AdicionarCredito(double valor)
   {
+    if (valor <= 0)
+      throw new ArgumentException("O valor deve ser positivo");
     Extrato += valor;
   }
 }
