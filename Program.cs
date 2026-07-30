ca2025dbf6f5fc898d528d3b8d9fbc7c9fa6e814
brunoconcli <bruno.concli@becomex.com.br>
2026-07-30 19:34:39 -0300

    fix: Redução do Program.cs para apenas inicializar a aplicação

--- Program.cs
+++ Program.cs
@@ -1,77 +1,28 @@
-﻿namespace RUtatouille;
+using Controllers;
+using Repositories;
 
-class Refeicao
-{
-  public string? principal;
-  public string? sobremesa;
-  public string? suco;
-}
-
-class Usuario
-{
-  public string? nome;
-  public string? codigo;
-  public double extrato;
-}
+namespace RUtatouille;
 
 class Program
 {
   public static void Main(string[] args)
   {
-    // Set up console styling
-    Console.Title = "Basic User Interaction App";
-    Console.ForegroundColor = ConsoleColor.Cyan;
+    var usuarioRepository = new InMemoryUsuarioRepository();
+    var refeicaoRepository = new InMemoryRefeicaoRepository();
+    var reservaRepository = new InMemoryReservaRepository();
 
-    Console.WriteLine(
-      "==================================\n" +
-      "\tRUtatouille\t\n" +
-      "==================================\n"
+    var usuarioController = new UsuarioController(usuarioRepository);
+    var refeicaoController = new RefeicaoController(
+      refeicaoRepository,
+      usuarioRepository,
+      reservaRepository
     );
-    Console.ResetColor();
-    var refeicoes = new Dictionary<string, Refeicao>
-    {
-      {"segunda", new Refeicao() {principal="bife a caçarola", sobremesa="paçoca", suco="uva"}},
-      {"terca", new Refeicao() {principal="franguinho", sobremesa="sagu de abacaxi", suco="laranja"}},
-    };
 
-    var usuarios = new Dictionary<string, Usuario>
-    {
-      {"123", new Usuario() { nome = "bruno", codigo = "123", extrato = 12.5 }}
-    };
+    var aplicacaoController = new AplicacaoController(
+      usuarioController,
+      refeicaoController
+    );
 
-    var codigosValidos = new Dictionary<string, string>
-    {
-      {"123", "Bruno"},
-      {"456", "Caio"},
-      {"789", "Vini"}
-    };
-    bool isCodigoValido = false;
-    var userName = "";
-
-    while (!isCodigoValido)
-    {
-      Console.Clear();
-      Console.Write("Bem vindo\nInsira seu codigo: ");
-      string codigo = Console.ReadLine() ?? "";
-
-      if (codigosValidos.ContainsKey(codigo))
-      {
-        isCodigoValido = true;
-        userName = codigosValidos[codigo];
-      }
-      else
-      {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("Error: Please enter a valid positive number.");
-        Console.ResetColor();
-      }
-    }
-    Console.WriteLine();
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"Olá, {userName}!");
-    Console.ResetColor();
-
-    Console.WriteLine("\n[ENTER] para deixar a aplicação...");
-    Console.ReadKey();
+    aplicacaoController.Executar();
   }
 }
